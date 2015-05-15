79818a69b092376ed468c6073282e2c025c809dc
Martin Fiebig <mfi@hacon.de>
2015-05-15 12:07:45 +0200

    Imported user code snippets

---
+++ property_assign.m
@@ -0,0 +1,10 @@
+// Property Assign
+// 
+//
+// IDECodeSnippetCompletionPrefix: ppa
+// IDECodeSnippetCompletionScopes: [All]
+// IDECodeSnippetIdentifier: 592423D0-F107-48E0-9EAD-CADD205DDD07
+// IDECodeSnippetLanguage: Xcode.SourceCodeLanguage.Objective-C
+// IDECodeSnippetUserSnippet: 1
+// IDECodeSnippetVersion: 2
+@property (nonatomic, assign) <#class#> <#name#>;
